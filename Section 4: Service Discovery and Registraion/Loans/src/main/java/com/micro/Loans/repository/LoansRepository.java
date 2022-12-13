9633cac8e619a720dd5dbccfaf66a5eb57173a91
sonisg <38506968+sonisg@users.noreply.github.com>
2022-12-13 16:52:26 +0530

    Add files via upload

---
+++ Section 4: Service Discovery and Registraion/Loans/src/main/java/com/micro/Loans/repository/LoansRepository.java
@@ -0,0 +1,15 @@
+package com.micro.Loans.repository;
+
+import com.micro.Loans.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+
+
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+}
