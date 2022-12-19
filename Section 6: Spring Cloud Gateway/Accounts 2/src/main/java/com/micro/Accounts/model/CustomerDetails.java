5759e52c4f60f2008bfeea7223944bdcfff27a65
sonisg <38506968+sonisg@users.noreply.github.com>
2022-12-19 11:50:07 +0530

    Add files via upload

---
+++ Section 6: Spring Cloud Gateway/Accounts 2/src/main/java/com/micro/Accounts/model/CustomerDetails.java
@@ -0,0 +1,19 @@
+package com.micro.Accounts.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class CustomerDetails {
+
+    private Accounts accounts;
+    private List<Loans> loans;
+    private List<Cards> cards;
+
+}
