3c84f0494dda708806a86ebc2108aa0589aa1621
sonisg <38506968+sonisg@users.noreply.github.com>
2022-12-19 11:50:48 +0530

    Add files via upload

---
+++ Section 6: Spring Cloud Gateway/cards/src/main/java/com/micro/cards/controller/CardsController.java
@@ -0,0 +1,49 @@
+package com.micro.cards.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.micro.cards.config.CardsConfig;
+import com.micro.cards.model.Cards;
+import com.micro.cards.model.Customer;
+import com.micro.cards.model.Properties;
+import com.micro.cards.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @Autowired
+    private CardsConfig cardsConfig;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
+}
