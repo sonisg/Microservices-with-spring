f933ba810a30b0d8079ba864f682a34f110ce24a
sonisg <38506968+sonisg@users.noreply.github.com>
2022-12-26 12:05:43 +0530

    Add files via upload

---
+++ Section 7: Distributed Traces/cards/src/main/java/com/micro/cards/controller/CardsController.java
@@ -0,0 +1,54 @@
+package com.micro.cards.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.micro.cards.config.CardsConfig;
+import com.micro.cards.model.Cards;
+import com.micro.cards.model.Customer;
+import com.micro.cards.model.Properties;
+import com.micro.cards.repository.CardsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @Autowired
+    private CardsConfig cardsConfig;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
+        logger.info("getCardDetails()");
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+                cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
+}
