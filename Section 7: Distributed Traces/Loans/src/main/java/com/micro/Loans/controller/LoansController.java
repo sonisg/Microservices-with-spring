17da61f825637c70ad13916e5a14eb1e5b2298ba
sonisg <38506968+sonisg@users.noreply.github.com>
2023-01-26 14:39:42 +0530

    Add files via upload

---
+++ Section 7: Distributed Traces/Loans/src/main/java/com/micro/Loans/controller/LoansController.java
@@ -0,0 +1,57 @@
+
+ package com.micro.Loans.controller;
+
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.micro.Loans.config.LoansConfig;
+import com.micro.Loans.model.Customer;
+        import com.micro.Loans.model.Loans;
+import com.micro.Loans.model.Properties;
+import com.micro.Loans.repository.LoansRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LoansController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
+
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @Autowired
+    private LoansConfig loansConfig;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoansDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
+
+        logger.info("getLoansDetails()");
+        List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+        if (loans != null) {
+            return loans;
+        } else {
+            return null;
+        }
+
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
+    }
+
+}
